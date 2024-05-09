494726ecc418785ef0e6eb0de29de322c188a2bb
amirali <167849011+amirali-khalili@users.noreply.github.com>
2024-05-09 14:35:18 +0330

    Update settings.py

--- persons/persons/settings.py
+++ persons/persons/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ticket',
-    'schools',
- ]
+    'schools'
+]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
